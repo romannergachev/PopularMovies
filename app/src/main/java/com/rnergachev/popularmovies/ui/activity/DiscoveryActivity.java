0fc16fbdb59284d99ae1c2ee4b17a1f4cfab6535
Roman <nergachev.roman@gmail.com>
2017-01-29 00:19:01 +0200

    add spinner, sort types, use apis

--- app/src/main/java/com/rnergachev/popularmovies/ui/activity/DiscoveryActivity.java
+++ app/src/main/java/com/rnergachev/popularmovies/ui/activity/DiscoveryActivity.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.rnergachev.popularmovies.BuildConfig;
 import com.rnergachev.popularmovies.R;
 import com.rnergachev.popularmovies.data.model.Movie;
 import com.rnergachev.popularmovies.ui.adapter.DiscoveryAdapter;
-import com.rnergachev.popularmovies.ui.presenter.DiscoveryActivityPresenter;
+import com.rnergachev.popularmovies.ui.adapter.EndlessRecyclerViewScrollListener;
 
 /**
  * Activity with the list of the movies
@@ -19,52 +25,109 @@ import com.rnergachev.popularmovies.ui.presenter.DiscoveryActivityPresenter;
  * Created by rnergachev on 27/01/2017.
  */
 
-public class DiscoveryActivity extends AppCompatActivity implements DiscoveryAdapter.DiscoveryAdapterOnClickHandler{
-    private DiscoveryActivityPresenter presenter;
-
+public class DiscoveryActivity
+    extends AppCompatActivity
+    implements DiscoveryAdapter.DiscoveryAdapterHandler, AdapterView.OnItemSelectedListener
+{
     private RecyclerView movieRecyclerView;
     private DiscoveryAdapter discoveryAdapter;
-    private TextView mErrorMessageDisplay;
-    private ProgressBar mLoadingIndicator;
+    private TextView errorMessageDisplay;
+    private ProgressBar loadingIndicator;
+    private EndlessRecyclerViewScrollListener scrollListener;
+    private GridLayoutManager gridLayoutManager;
+    private int currentSort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discovery);
-        presenter = new DiscoveryActivityPresenter(getString(R.string.base_url));
 
-        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
-        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        Toolbar toolbar     = (Toolbar) findViewById(R.id.my_toolbar);
+        errorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+        loadingIndicator    = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        movieRecyclerView   = (RecyclerView) findViewById(R.id.movies_list);
+        Spinner spinner     = (Spinner) findViewById(R.id.spinner_sort);
 
-        discoveryAdapter = new DiscoveryAdapter(this, this);
+        discoveryAdapter    = new DiscoveryAdapter(this, this, true);
+        gridLayoutManager   = new GridLayoutManager(this, BuildConfig.NUMBER_OF_COLUMNS);
 
-        movieRecyclerView = (RecyclerView) findViewById(R.id.movies_list);
-        movieRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        movieRecyclerView.setHasFixedSize(true);
+        setSupportActionBar(toolbar);
+        try {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        } catch (NullPointerException ex) {
+            Log.d(getClass().getName(), "Cannot disable the title!");
+        }
+
+        currentSort = 0;
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.sort_array,
+            android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        movieRecyclerView.setLayoutManager(gridLayoutManager);
         movieRecyclerView.setAdapter(discoveryAdapter);
+        scrollListener = new EndlessRecyclerViewScrollListener(gridLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                discoveryAdapter.fetchMovies();
+            }
+        };
+        movieRecyclerView.addOnScrollListener(scrollListener);
 
-        //loadWeatherData();
+        discoveryAdapter.fetchMovies();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        presenter.fetchPopularMovies();
     }
 
     @Override
     public void onClick(Movie movie) {
-        //// TODO: 28.1.2017
+        Log.d(getClass().getName(), "CLICK");
     }
 
-    private void showWeatherDataView() {
-        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+    @Override
+    public void onError(Throwable exception) {
+        onFetchingEnded();
+        Log.d(getClass().getName(), exception.getMessage());
+        movieRecyclerView.setVisibility(View.INVISIBLE);
+        errorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onDismissError() {
+        errorMessageDisplay.setVisibility(View.INVISIBLE);
         movieRecyclerView.setVisibility(View.VISIBLE);
     }
 
+    @Override
+    public void onFetchingStarted() {
+        loadingIndicator.setVisibility(View.VISIBLE);
+    }
 
-    private void showErrorMessage() {
-        movieRecyclerView.setVisibility(View.INVISIBLE);
-        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    @Override
+    public void onFetchingEnded() {
+        loadingIndicator.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (currentSort == position) {
+            return;
+        }
+        gridLayoutManager.removeAllViews();
+        currentSort = position;
+        discoveryAdapter.clearAdapter(position == 0);
+        discoveryAdapter.fetchMovies();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 }
--- app/src/main/java/com/rnergachev/popularmovies/ui/adapter/DiscoveryAdapter.java
+++ app/src/main/java/com/rnergachev/popularmovies/ui/adapter/DiscoveryAdapter.java
@@ -2,6 +2,7 @@ package com.rnergachev.popularmovies.ui.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,44 +10,63 @@ import android.widget.ImageView;
 
 import com.rnergachev.popularmovies.R;
 import com.rnergachev.popularmovies.data.model.Movie;
+import com.rnergachev.popularmovies.data.model.MoviesResponse;
+import com.rnergachev.popularmovies.data.network.MovieApi;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Observable;
+
 /**
+ * Adapter class for Movies list
+ *
  * Created by roman on 28.1.2017.
  */
 
 public class DiscoveryAdapter extends RecyclerView.Adapter<DiscoveryAdapter.DiscoveryAdapterViewHolder> {
     private List<Movie> movieList;
     private int currentPage;
+    private int maxPage;
     private Context context;
-    private final DiscoveryAdapterOnClickHandler clickHandler;
+    private final DiscoveryAdapterHandler handler;
+    private boolean isPopularSort;
+    private MovieApi movieApi;
 
-    public DiscoveryAdapter(Context context, DiscoveryAdapterOnClickHandler clickHandler) {
-        currentPage = 1;
+    public DiscoveryAdapter(Context context, DiscoveryAdapterHandler handler, boolean isPopularSort) {
+        currentPage = 0;
+        maxPage = Integer.MAX_VALUE;
         this.context = context;
-        this.clickHandler = clickHandler;
+        this.handler = handler;
+        movieApi = new MovieApi(context.getString(R.string.base_url));
+        movieList = new ArrayList<>();
+        this.isPopularSort = isPopularSort;
     }
 
-    public interface DiscoveryAdapterOnClickHandler {
+    public interface DiscoveryAdapterHandler {
         void onClick(Movie movie);
+        void onError(Throwable exception);
+        void onFetchingStarted();
+        void onFetchingEnded();
+        void onDismissError();
     }
 
-    public class DiscoveryAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    class DiscoveryAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public final ImageView movieThumbnail;
+        private final ImageView movieThumbnail;
 
-        public DiscoveryAdapterViewHolder(View view) {
+        DiscoveryAdapterViewHolder(View view) {
             super(view);
             movieThumbnail = (ImageView) view.findViewById(R.id.movie_thumbnail_image_view);
+            view.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
             int adapterPosition = getAdapterPosition();
             Movie movie = movieList.get(adapterPosition);
-            clickHandler.onClick(movie);
+            handler.onClick(movie);
         }
     }
 
@@ -59,8 +79,7 @@ public class DiscoveryAdapter extends RecyclerView.Adapter<DiscoveryAdapter.Disc
     @Override
     public void onBindViewHolder(DiscoveryAdapterViewHolder holder, int position) {
         String movieUrl = movieList.get(position).getPosterPath();
-        StringBuilder url = new StringBuilder(context.getString(R.string.image_base_url)).append(movieUrl);
-        Picasso.with(context).load(url.toString()).into(holder.movieThumbnail);
+        Picasso.with(context).load(context.getString(R.string.image_base_url) + movieUrl).into(holder.movieThumbnail);
     }
 
     @Override
@@ -69,8 +88,37 @@ public class DiscoveryAdapter extends RecyclerView.Adapter<DiscoveryAdapter.Disc
         return movieList.size();
     }
 
-    public void setWeatherData(List<Movie> movieList) {
-        this.movieList = movieList;
-        notifyDataSetChanged();
+    public void fetchMovies() {
+        if (currentPage == maxPage) {
+            return;
+        }
+        handler.onDismissError();
+        handler.onFetchingStarted();
+        currentPage++;
+        Log.d(getClass().getName(), "Fetching page: " + currentPage);
+        Observable<MoviesResponse> request;
+        if (isPopularSort) {
+            request = movieApi.popularMovies(currentPage);
+        } else {
+            request = movieApi.topRatedMovies(currentPage);
+        }
+
+        request.subscribe(
+            response -> {
+                movieList.addAll(response.getMovies());
+                this.notifyDataSetChanged();
+                maxPage = response.getTotalPages();
+                handler.onFetchingEnded();
+            },
+            handler::onError
+        );
+
+    }
+
+    public void clearAdapter(boolean isPopularSort) {
+        currentPage = 0;
+        maxPage = Integer.MAX_VALUE;
+        movieList = new ArrayList<>();
+        this.isPopularSort = isPopularSort;
     }
 }
