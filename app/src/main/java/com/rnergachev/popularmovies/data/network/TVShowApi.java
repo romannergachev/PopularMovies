fc37bf3544ac06bae3adab0949ea2063845b472b
Roman Nergachev <nergachev.roman@gmail.com>
2017-05-31 17:33:30 +0300

    feat: Implement retrofit API for TV Shows

--- app/src/main/java/com/rnergachev/popularmovies/data/network/TVShowApi.java
+++ app/src/main/java/com/rnergachev/popularmovies/data/network/TVShowApi.java
@@ -1,11 +1,7 @@
 package com.rnergachev.popularmovies.data.network;
 
 import com.rnergachev.popularmovies.BuildConfig;
-import com.rnergachev.popularmovies.data.model.Movie;
-import com.rnergachev.popularmovies.data.model.MoviesResponse;
-import com.rnergachev.popularmovies.data.model.ReviewsResponse;
 import com.rnergachev.popularmovies.data.model.TVShowResponse;
-import com.rnergachev.popularmovies.data.model.TrailersResponse;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -15,38 +11,18 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 /**
- * Movie API calling MovieDbService
+ * Movie API calling TVShowDbService
  *
  * Created by rnergachev on 27/01/2017.
  */
 
 @Singleton
-public class MovieApi {
-    private MovieDbService movieDbService;
+public class TVShowApi {
+    private TVShowDbService TVShowDbService;
 
     @Inject
-    public MovieApi(MovieDbService movieDbService) {
-        this.movieDbService = movieDbService;
-    }
-
-    /**
-     * Returns popular movies as a Single object
-     *
-     * @param  page   number of current page
-     * @return        {@link Single<MoviesResponse>}
-     */
-    public Single<MoviesResponse> popularMovies(int page) {
-        return applySchedulers(movieDbService.popularMovies(BuildConfig.THE_MOVIE_DB_API_KEY, page));
-    }
-
-    /**
-     * Returns top rated movies as a Single object
-     *
-     * @param  page   number of current page
-     * @return        {@link Single<MoviesResponse>}
-     */
-    public Single<MoviesResponse> topRatedMovies(int page) {
-        return applySchedulers(movieDbService.topRatedMovies(BuildConfig.THE_MOVIE_DB_API_KEY, page));
+    public TVShowApi(TVShowDbService TVShowDbService) {
+        this.TVShowDbService = TVShowDbService;
     }
 
     /**
@@ -56,44 +32,24 @@ public class MovieApi {
      * @return        {@link Single<TVShowResponse>}
      */
     public Single<TVShowResponse> topRatedTVShows(int page) {
-        return applySchedulers(movieDbService.topRatedTVShows(BuildConfig.THE_MOVIE_DB_API_KEY, page));
+        return applySchedulers(TVShowDbService.topRatedTVShows(BuildConfig.THE_MOVIE_DB_API_KEY, page));
     }
 
     /**
      * Returns similar tv shows as a Single object
      *
-     * @param  tvId   movie id
+     * @param  tvId   tv show id
      * @return        {@link Single<TVShowResponse>}
      */
     public Single<TVShowResponse> similarTVShows(int tvId, int page) {
-        return applySchedulers(movieDbService.similarTVShows(tvId, BuildConfig.THE_MOVIE_DB_API_KEY, page));
-    }
-
-    /**
-     * Returns trailers as a Single object
-     *
-     * @param  movieId   movie id
-     * @return        {@link Single<TrailersResponse>}
-     */
-    public Single<TrailersResponse> getTrailers(int movieId) {
-        return applySchedulers(movieDbService.getTrailers(movieId, BuildConfig.THE_MOVIE_DB_API_KEY));
-    }
-
-    /**
-     * Returns reviews as a Single object
-     *
-     * @param  movieId   movie id
-     * @return        {@link Single<ReviewsResponse>}
-     */
-    public Single<ReviewsResponse> getReviews(int movieId) {
-        return applySchedulers(movieDbService.getReviews(movieId, BuildConfig.THE_MOVIE_DB_API_KEY));
+        return applySchedulers(TVShowDbService.similarTVShows(tvId, BuildConfig.THE_MOVIE_DB_API_KEY, page));
     }
 
     /**
      * Adds schedulers to the call
      *
      * @param  single object to add schedulers
-     * @return        {@link Single<MoviesResponse>}
+     * @return        {@link Single<T>}
      */
     private <T> Single<T> applySchedulers(Single<T> single) {
         return single.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
